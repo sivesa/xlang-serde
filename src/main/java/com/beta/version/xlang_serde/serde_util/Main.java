0e77d6261cb36916146f8836e25a0d9ba373a508
Sive <sivesandla@live.com>
2025-01-07 22:53:02 +0200

    Adding Main class and pom files.

---
+++ src/main/java/com/beta/version/xlang_serde/serde_util/Main.java
@@ -0,0 +1,13 @@
+package com.beta.version.xlang_serde.serde_util;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            // Create a sample objec
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
---
+++ src/test/java/com/beta/version/xlang_serde/serde_util/MainTests.java
@@ -0,0 +1,13 @@
+package com.beta.version.xlang_serde.serde_util;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerdeUtilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
